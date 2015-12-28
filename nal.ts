f3762481aed5790d9a974ae488141eab46398a25
SeH <seh999@gmail.com>
2015-12-28 09:50:17 -0500

    dfsdfsdf

--- nal.ts
+++ nal.ts
@@ -1,92 +1,204 @@
-/**
- * Created by me on 12/28/15.
- */
-const $ = {
+"use strict";
 
-};
+import imm = require('mori');
+
+module nal {
+
+    /** term types */
+    const T = new Array(16);
 
 
 
-class Truth {
+    class TType {
+        sym:string;
 
-    /** frequency [0..1.0] */
-    f: number;
+        constructor(sym:string) {
+            this.sym = sym;
+        }
 
-    /** confidence [0..1.0] */
-    c: number;
+        static numTypes:number = 0;
 
-    constructor(freq:number, conf:number) {
-        this.f = freq;
-        this.c = conf;
+        //toString() { return this.sym; }
+
     }
 
-    /** expectation postive, ie. the expectation of freq=1 */
-    expPos():number {
-        return this.expectation(this.f);
+    function initType(t:(x:number)=>TType):Symbol {
+        var n = TType.numTypes++;
+        var s = Symbol(n);
+        T[n] = t(n);
+        return s;
     }
 
-    /** expectation inverse, ie. the expectation of freq=0 */
-    expNeg():number {
-        return this.expectation(1.0-this.f);
+
+    const ATOM = initType((n)=> {
+        return new TType('.');
+    });
+    const INHERIT = initType((n)=> {
+        return new TType('-->');
+    });
+    const SIMILAR = initType((n)=> {
+        return new TType('<->');
+    });
+
+    console.log("Types\n",T);
+
+//
+//    ATOM: {
+//        sym: '.',
+//        subsMin: 0, subsMax: 0
+//    },
+//
+//    INHERIT: {
+//
+//    },
+//
+//    SIMILAR: {
+//
+//    }
+//
+//
+//
+//    //..
+//}
+
+    /** scalar unit truth measurement: freq,conf */
+    class Truth {
+
+        /** frequency [0..1.0] */
+        f:number;
+
+        /** confidence [0..1.0] */
+        c:number;
+
+        constructor(freq:number, conf:number) {
+            this.f = freq;
+            this.c = conf;
+        }
+
+        /** expectation postive, ie. the expectation of freq=1 */
+        expPos():number {
+            return this.expectation(this.f);
+        }
+
+        /** expectation inverse, ie. the expectation of freq=0 */
+        expNeg():number {
+            return this.expectation(1.0 - this.f);
+        }
+
+        expectation(frequency:number):number {
+            return (this.c * (frequency - 0.5) + 0.5);
+        }
+
+        static defaultBelief:Truth = new Truth(1.0, 0.9);
+        static defaultGoal:Truth = new Truth(1.0, 0.9);
     }
 
-    expectation(frequency:number):number  {
-        return (this.c * (frequency - 0.5) + 0.5);
+    /**
+     * energy (aka budget) vector
+     *
+     * analogous to voltage or pressure
+     *
+     * one way to interpret is as a 3D polar coordinate:
+     *      radius/magnitude = priority
+     *      angles = durability, quality
+     */
+    class Energy {
+        /** priority/activation/charge/temperature */
+        p:number;
+        /** durability */
+        d:number;
+        /** quality */
+        q:number;
     }
 
-    static defaultBelief:Truth = new Truth(1.0, 0.9);
-    static defaultGoal:Truth = new Truth(1.0, 0.9);
-}
+    /**
+     energized state vector
 
-class Budget {
+     energy vector && delta-energy vector
 
-}
+     analogous to capacitance
+     */
+    class Energized extends Energy {
+        /** timestamp of last update */
+        updateLast:number;
 
-/** task punctuation */
-enum Punc {
-    /** belief (aka judgment) */
-    B,
+        /** change in priority to apply next update */
+        dp:number;
 
-    /** goal (aka desire) */
-    G,
+        /** change in durability to apply next update */
+        dd:number;
 
-    /** question */
-    Q,
-
-    /** quest */
-    W,
-
-    /** command */
-    C
-}
-
-interface Term {
-
-}
-interface Concept {
-
-}
-interface Task {
+        /** change in quality to apply next update */
+        dq:number;
+    }
 
 
-}
-class NAR extends Map<Term,Concept> {
+    /** task punctuation */
+    const enum Punc {
+
+        /**
+         b = believe judgment
+         g = desire goal
+         q = question (what/why/who/when/where/how...)
+         w = quest (will i)
+         */
+
+        /** belief (aka judgment) */
+        B,
+
+        /** goal (aka desire) */
+        G,
+
+        /** question */
+        Q,
+
+        /** quest */
+        W,
+
+        /** command */
+        C
+    }
+
+    interface Term {
+        id: (string | Object)
+    }
+
+    interface Concept {
+        id: Term
+    }
+
+    interface Task {
+        term: Term
+
+    }
+
+    class Bag {
+        capacity: number
+
+    }
+
+    class Index /* extends Map<Term,Concept> */ {
+
+        get(t:Term):Concept {
+            return null;
+        }
+
+        computeIfAbsent(t:Term, conceptBuilder:(x:Term)=>Concept):Concept {
+            return null;
+        }
+
+    }
+
+    /** Non-Axiomatic Reasoner (NAR) */
+    class NAR extends Index {
 
 
-    /** input task */
-    in(term: Term, truth: Truth, punc:Punc):Task {
-        //return in(new Task(term, truth, punc));
-        return null;
+        /** input task */
+        in(term:Term, truth:Truth, punc:Punc):Task {
+            //return in(new Task(term, truth, punc));
+            return null;
+        }
+
     }
 
 }
-
-/**
- b = believe judgment
- g = desire goal
- q = question (what/why/who/when/where/how...)
- w = quest (will i)
- */
-
-
-//believe(inh('a', 'b'));
